87204ffd7192cb9f29b3fef68a1aed31d1d9177d
simsonparajuli <89746159+simsonparajuli@users.noreply.github.com>
2021-09-10 12:26:15 -0600

    Update Week2Part2.java

--- Week2JavaReview/Week2Part2.java
+++ Week2JavaReview/Week2Part2.java
@@ -1,1 +1,31 @@
+package javareview;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Week2Part2 {
+	public static void main(String[] args) throws IOException {
+
+	BufferedReader br = new BufferedReader(new FileReader("src/javareview/text.txt"));
+	String str;
+	int counter = 0;
+	Float sum =0f;
+   while ((str = br.readLine()) != null) {
+	   // Counting number of Rows
+	   counter++;
+	   // Converting String value to Float Value
+	   Float fvalue = Float.parseFloat(str);
+	  // adding all float values
+	   sum = sum + fvalue;
+   }
+   br.close();
+   // average 
+   float avg = sum / counter;
+   
+   // Printing user-friendly Statement
+   System.out.printf("Number of Rows in the file: %d\n",counter);
+   System.out.printf("Total of all of the numbers: %.2f\n",sum);
+   System.out.printf("The average number in the file: %.2f",avg);
+}
+}
