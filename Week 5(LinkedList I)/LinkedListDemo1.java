fa310c56c6fb3a81fc705254f5e2868ff249332a
simsonparajuli <89746159+simsonparajuli@users.noreply.github.com>
2021-11-26 10:43:59 -0700

    Update LinkedListDemo1.java

--- Week 5(LinkedList I)/LinkedListDemo1.java
+++ Week 5(LinkedList I)/LinkedListDemo1.java
@@ -1,1 +1,43 @@
 
+package Linkedlist;
+
+public class LinkedListDemo1 {
+	
+	 Node head;
+	 
+	public static void main(String[] args) {
+	LinkedList ll = new LinkedList();
+	
+	ll.append(2);
+	
+	ll.append(45);
+	
+	ll.append(43);
+	
+	ll.append(5);
+	
+	ll.append(45);
+	
+	ll.append(23);
+	
+	ll.append(34);
+
+	ll.Prepend(120);
+	
+	ll.Delete(7);
+	
+	ll.append(3);
+	
+	
+	ll.Delete(0);
+	
+	
+	ll.Search(23);
+	
+	ll.Length();
+	
+	
+	ll.printList();
+
+ }
+}
