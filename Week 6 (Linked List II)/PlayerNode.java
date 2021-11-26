932d6d14e35e70642dbeddf490cce489c93651d4
simsonparajuli <89746159+simsonparajuli@users.noreply.github.com>
2021-11-26 10:48:31 -0700

    Update PlayerNode.java

--- Week 6 (Linked List II)/PlayerNode.java
+++ Week 6 (Linked List II)/PlayerNode.java
@@ -1,1 +1,27 @@
 
+package LinkedListII;
+
+public class PlayerNode {
+      PlayerNode next;
+	
+      int id;
+      String firstName;
+      String lastName;
+      String playerName;
+      String playerType;
+      double lifePoints;
+      int totalScore;
+	
+	
+	public PlayerNode(int id,String firstName, String lastName, String playerName, String playerType,
+			double lifePoints, int totalScore) {
+	    this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.playerName = playerName;
+      this.playerType = playerType;
+      this.lifePoints = lifePoints;
+      this.totalScore = totalScore;
+	}	
+
+}
