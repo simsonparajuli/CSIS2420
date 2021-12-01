54465e9a8dac475d5bdccba56865b02d20315901
simsonparajuli <89746159+simsonparajuli@users.noreply.github.com>
2021-12-01 12:18:11 -0700

    Add files via upload

---
+++ week 9 (Tree - II)/BSTNode.java
@@ -0,0 +1,17 @@
+package BinarySearchTree;
+
+public class BSTNode {
+
+	int ip;
+	String userName;
+	BSTNode left;
+	BSTNode right;
+
+
+	 public BSTNode(int ip,String userName) {
+		 this.ip = ip;
+		 this.userName = userName;
+	  }
+	 
+	 
+ }
